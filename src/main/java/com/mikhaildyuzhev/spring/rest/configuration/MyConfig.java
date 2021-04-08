295f9e54dfafb9443e80c18810f7073a01da7b5d
mdyuzhev <Misha2016>
2021-04-09 02:30:30 +0300

    data source

---
+++ src/main/java/com/mikhaildyuzhev/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,36 @@
+package com.mikhaildyuzhev.spring.rest.configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+
+@Configuration
+@ComponentScan(basePackages = "com.mikhaildyuzhev.spring.rest")
+@EnableWebMvc
+public class MyConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&amp;serverTimezone=UTC");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+
+        return dataSource;
+    }
+
+
+
+
+}
