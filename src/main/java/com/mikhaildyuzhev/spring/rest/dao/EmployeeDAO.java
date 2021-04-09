6837823c03dc8116206525c0ff218111438a9bf1
mdyuzhev <Misha2016>
2021-04-09 12:29:22 +0300

    HTTP Status 500

---
+++ src/main/java/com/mikhaildyuzhev/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.mikhaildyuzhev.spring.rest.dao;
+
+import com.mikhaildyuzhev.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
---
+++ src/main/java/com/mikhaildyuzhev/spring/rest/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.mikhaildyuzhev.spring.rest.service;
+import com.mikhaildyuzhev.spring.rest.entity.Employee;
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee (Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
